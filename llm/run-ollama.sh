60f26ff67f68227eaa921cba75b55d47c82e5c17
g3yk <g3yk@users.noreply.github.com>
2025-04-27 22:29:27 +0300

    feat(llm): Add llm service with deepseek-r1:1.5b model

---
+++ llm/run-ollama.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo "Starting Ollama server..."
+ollama serve &
+
+
+echo "Waiting for Ollama server to be active..."
+while [ "$(ollama list | grep 'NAME')" == "" ]; do
+  sleep 1
+done
+
+ollama pull deepseek-r1:1.5b
